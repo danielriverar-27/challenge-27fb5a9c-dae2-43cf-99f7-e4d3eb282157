12b13e3c016b5b44268d5225204da2dbcc85cac8
Daniel Rivera <daniel.riverar@pragma.com.co>
2026-09-16 15:37:12 -0500

    feat: phase 1

--- src/core/correlation-strategy.ts
+++ src/core/correlation-strategy.ts
@@ -1,3 +1,6 @@
+import { Injectable } from '@nestjs/common';
+import { Observable, of } from 'rxjs';
+
 @Injectable()
 export class CorrelationStrategy {
   correlateEvents(event: any): Observable<any> {
---
+++ src/main/app.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { ObservableController } from '../api/observable-controller';
+import { ObservableService } from '../api/observable-service';
+import { CorrelationStrategy } from '../core/correlation-strategy';
+import { ReactiveObservability } from '../infrastructure/reactive-observability';
+import { ProactiveObservability } from '../infrastructure/proactive-observability';
+
+@Module({
+  imports: [],
+  controllers: [ObservableController],
+  providers: [
+    ObservableService,
+    CorrelationStrategy,
+    ReactiveObservability,
+    ProactiveObservability,
+  ],
+})
+export class AppModule {}
